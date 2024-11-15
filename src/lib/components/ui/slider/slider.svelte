3b2d64374a36b0401df563baa903c0311e5b8479
Christian.Kornfeld <christian.kornfeld@euvic.at>
2024-11-15 15:15:46 +0100

    Update slider component to handle touch and pointer events
    Refactor slider event handling to manage touch and pointer interactions separately. This change improves user experience by blocking unintended Y-axis movements and ensures proper overlay toggling during these interactions. Also, update the package version to 2.2.6 for maintaining versioning consistency.

--- src/lib/components/ui/slider/slider.svelte
+++ src/lib/components/ui/slider/slider.svelte
@@ -11,17 +11,52 @@
 	export { className as class };
 
 	let showOverlay = writable(false);
+	let startTouchX = writable(0);
+	let startTouchY = writable(0);
+
+	let startPointerX = writable(0);
+	let startPointerY = writable(0);
 
 	onMount(() => {});
 
-	const pointerDown = () => {
-		console.log('pointerdown');
-		showOverlay.set(true);
+	const handleTouchEnd = (event: Event) => {
+		showOverlay.set(false);
 	};
 
-	const pointerUp = () => {
-		console.log('pointerup');
+	const handlePointerEnd = (event: Event) => {
 		showOverlay.set(false);
+	};
+
+	const handleTouchMove = (event: TouchEvent) => {
+		const touch = event.touches[0];
+		const deltaY = Math.abs(touch.clientY - $startTouchY);
+		if (deltaY > 2) {
+			event.preventDefault(); // Block Y-axis movements
+			event.stopPropagation();
+		}
+	};
+
+	const handlePointerMove = (event: PointerEvent) => {
+		const deltaY = Math.abs(event.clientY - $startPointerY);
+		if (deltaY > 2) {
+			event.preventDefault(); // Block Y-axis movements
+			event.stopPropagation();
+		}
+	};
+
+	const handleTouchStart = (event: TouchEvent) => {
+		const touch = event.touches[0];
+		startTouchX.set(touch.clientX);
+		startTouchY.set(touch.clientY);
+		showOverlay.set(true);
+		event.preventDefault(); // Ensure it stops propagation here
+	};
+
+	const handlePointerStart = (event: PointerEvent) => {
+		startPointerX.set(event.clientX);
+		startPointerY.set(event.clientY);
+		showOverlay.set(true);
+		event.preventDefault(); // Ensure it stops propagation here
 	};
 </script>
 
@@ -40,6 +75,12 @@
 	class={cn('relative flex w-full touch-none select-none items-center', className)}
 	{...$$restProps}
 	let:thumbs
+	ontouchstart={(e) => handleTouchStart(e)}
+	ontouchmove={(e) => handleTouchMove(e)}
+	ontouchend={(e) => handleTouchEnd(e)}
+	onpointerdown={(e) => handlePointerStart(e)}
+	onpointermove={(e) => handlePointerMove(e)}
+	onpointerup={(e) => handlePointerEnd(e)}
 >
 	<span class="relative h-2 w-full grow overflow-hidden rounded-full bg-primary/20">
 		<SliderPrimitive.Range class="absolute h-full bg-primary" />
@@ -47,8 +88,6 @@
 	{#each thumbs as thumb}
 		<SliderPrimitive.Thumb
 			{thumb}
-			onpointerdown={() => pointerDown()}
-			onpointerup={() => pointerUp()}
 			class="block h-6 w-6 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
 		/>
 	{/each}
